9539d0134ba4e48be26a97515dba87076182a4fa
L1nar <122251664+L1nar@users.noreply.github.com>
2024-04-08 16:42:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Массив с вопросами, вариантами ответов и правильными ответами
+let questions = [
+    {
+        question: "Какой язык программирования вы изучаете?",
+        options: ['JavaScript', 'Python', 'PHP', 'C++'],
+        correctAnswer: 'JavaScript',
+    },
+    {
+        question: "Какой язык программирования вы изучаете?",
+        options: ['JavaScript', 'Python', 'PHP', 'C++'],
+        correctAnswer: 'JavaScript',
+    },
+    {
+        question: "Какой язык программирования вы изучаете?",
+        options: ['JavaScript', 'Python', 'PHP', 'C++'],
+        correctAnswer: 'JavaScript',
+    },
+]
+
+let currentQuestion = 0; // Текущий вопрос
+let correctAnswers = 0; // Кол-во правильных ответов
